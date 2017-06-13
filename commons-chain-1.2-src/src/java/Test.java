a0c62781427f6c6d3871a3a8792cec39b51418d9
wangxiaofei <wangxiaofei@corp.netease.com>
2017-06-13 18:00:18 +0800

    修改

--- commons-chain-1.2-src/src/java/Test.java
+++ commons-chain-1.2-src/src/java/Test.java
@@ -1,4 +1,7 @@
+import org.apache.commons.chain.Catalog;
+import org.apache.commons.chain.CatalogFactory;
 import org.apache.commons.chain.Context;
+import org.apache.commons.chain.impl.CatalogBase;
 import org.apache.commons.chain.impl.ContextBase;
 
 public class Test
@@ -7,6 +10,7 @@ public class Test
 	public static void main(String[] args) throws Exception
 	{
 		MyCommand command = new MyCommand();
+		System.out.println(command);
 		command.setMethod("sayHello");
 		Context context = new ContextBase();
 		context.put("say", "hello world!");
@@ -14,7 +18,11 @@ public class Test
 		command.execute(context);
 		command.setMethod("sayMorning");
 		command.execute(context);
-		//System.out.println(result);
+		CatalogFactory factory = CatalogFactory.getInstance();
+		Catalog catalog = new CatalogBase();
+		factory.addCatalog("say", catalog);
+		catalog.addCommand("myCommand", command);
+		System.out.println(catalog.getCommand("myCommand"));
 	}
 
 }
